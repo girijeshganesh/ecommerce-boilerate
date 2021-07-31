a154688c53b612e40d2ba0d97e68a597add7f223
giriui <giriuidevelop@gmail.com>
2021-07-31 23:30:37 +0530

    Project setup init

---
+++ ecommerce/ecommerce/src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  addOrder() {
+    $("#item_list").toggleClass("active");
+    $("#no-order").removeClass("active");
+  }
+  cancelOrder() {
+    $("#no-order").toggleClass("active");
+    $("#item_list").removeClass("active");
+  }
+  placeOrder() {
+    $("#amount_to_Pay").toggleClass("active");
+    $("#item_list").removeClass("active");
+  }
+  editorder() {
+    $("#item_list").toggleClass("active");
+    $("#amount_to_Pay").removeClass("active");
+  }
+}
